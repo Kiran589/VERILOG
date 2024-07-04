55d0fd9ef9ff6c01b6cdc1702d392c342ab326e5
Kiran589 <101051183+Kiran589@users.noreply.github.com>
2024-07-04 11:54:15 +0530

    Update Glitch_free_clk_mux_tb.v

--- Day4/Assignment1/Glitch_free_clk_mux_tb.v
+++ Day4/Assignment1/Glitch_free_clk_mux_tb.v
@@ -7,10 +7,8 @@ reg pclk_62_5mhz;
 reg pclk_250mhz;
 reg [1:0] rate;
 
-// Output
 wire pclk;
 
-// Instantiate the Unit Under Test (UUT)
 clock_mux uut (
     .pclk_62_5mhz(pclk_62_5mhz), 
     .pclk_250mhz(pclk_250mhz), 
@@ -19,25 +17,20 @@ clock_mux uut (
 );
 
 initial begin
-    // Initialize Inputs
     pclk_62_5mhz = 0;
     pclk_250mhz = 0;
     rate = 2'b00;
-
-    // Wait 100 ns for global reset to finish
-    #100;
-    
-    // Add stimulus here
-    forever #8 pclk_62_5mhz = ~pclk_62_5mhz; // Toggle every 8ns for 62.5MHz
-    forever #2 pclk_250mhz = ~pclk_250mhz; // Toggle every 2ns for 250MHz
+    #100
+    forever #8 pclk_62_5mhz = ~pclk_62_5mhz; 
+    forever #2 pclk_250mhz = ~pclk_250mhz; 
     
 end
 
 initial begin
-    #200; // Wait for clocks to stabilize
-    rate = 2'b00; // Select 62.5MHz clock
+    #200; 
+    rate = 2'b00; 
     #100;
-    rate = 2'b10; // Select 250MHz clock
+    rate = 2'b10; 
     #100;
      #2000 $stop; 
 end
